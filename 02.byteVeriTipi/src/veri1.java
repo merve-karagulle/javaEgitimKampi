e308da5f1ba04424d77c30d25ee9f719562d37e2
merve-karagulle <mervekrgll44@icloud.com>
2025-07-05 16:42:57 +0300

    Update veri1.java

--- 02.byteVeriTipi/src/veri1.java
+++ 02.byteVeriTipi/src/veri1.java
@@ -2,7 +2,7 @@
 public class veri1 {
 
 	public static void main(String[] args) {
-		//Byte veri tipi, -128 ile 127 arasında değer alır.
+		//Byte veri tipi, -128 ile 127 arasında değer almaktadır.
 		
 		byte sayi=125;
 		System.out.println(sayi);
