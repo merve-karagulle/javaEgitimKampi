db2995c45761e8cda3ca713b66e8d782c8cd2604
merve-karagulle <mervekrgll44@icloud.com>
2025-07-05 16:44:02 +0300

    Update veri2.java

--- 03.shortVeriTipi/src/veri2.java
+++ 03.shortVeriTipi/src/veri2.java
@@ -2,7 +2,7 @@
 public class veri2 {
 
 	public static void main(String[] args) {
-		// Short veri tipi -32768 ve 32767 arasında değerler alır.
+		// Short veri tipi -32768 ve 32767 arasında değerler almaktadır.
 		short sayi=1397;
 		System.out.println(sayi);
 	}
