102cbb782a984d97904726271a8a264b2b9eb529
merve-karagulle <mervekrgll44@icloud.com>
2025-07-05 16:44:45 +0300

    Update veri3.java

--- 04.integerVeriTipi/src/veri3.java
+++ 04.integerVeriTipi/src/veri3.java
@@ -2,7 +2,7 @@
 public class veri3 {
 
 	public static void main(String[] args) {
-		// 9 basamağa kadar olan tamsayıları tanımlamak için kullanılır.
+		// 9 basamağa kadar olan tamsayıları tanımlamak için kullanılmaktadır.
 		int sayi=13245;
 		int sayi2=1;
 	  System.out.println(sayi);
