455dd8e26ea6599bd2b21a877148d34523e97349
Merve Karagulle <mervekrgll44@icloud.com>
2025-07-06 09:11:30 +0300

    Update veri8.java

--- 09.charVeriTipi/veri8.java
+++ 09.charVeriTipi/veri8.java
@@ -2,7 +2,7 @@
 public class veri8 {
 
 	public static void main(String[] args) {
-		// karakter tanımlamak için kullanılır. Tanımında tek tırnak kullanılması zorunludur.
+		//Karakter tanımlamak için kullanılır. Tanımında tek tırnak kullanılması zorunludur.
 		char karakter='M';
 		System.out.println(karakter);
         char karakter2='?';
