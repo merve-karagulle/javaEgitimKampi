bee9b1ecf615ad6a35ee0627b3d9b3f0cc985cce
Merve Karagulle <mervekrgll44@icloud.com>
2025-07-06 09:10:14 +0300

    Add files via upload

---
+++ 11.atamaOperatoru/operator1.java
@@ -0,0 +1,12 @@
+
+public class operator1 {
+
+	public static void main(String[] args) {
+		// Atama operatörü = 'dir.
+		int sayi=22;
+		// eşitliğin sağ tarafındaki değeri al ve sol taraftaki değişken içerisine koy.
+		System.out.println(sayi);
+
+	}
+
+}
