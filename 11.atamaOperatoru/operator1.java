46507a43e142a7233e3ac97e1072d8477c2d04d3
Merve Karagulle <mervekrgll44@icloud.com>
2025-07-06 09:12:49 +0300

    Update operator1.java

--- 11.atamaOperatoru/operator1.java
+++ 11.atamaOperatoru/operator1.java
@@ -2,9 +2,9 @@
 public class operator1 {
 
 	public static void main(String[] args) {
-		// Atama operatörü = 'dir.
+		//Atama operatörü = 'dir.
 		int sayi=22;
-		// eşitliğin sağ tarafındaki değeri al ve sol taraftaki değişken içerisine koy.
+		//Eşitliğin sağ tarafındaki değeri al ve sol taraftaki değişken içerisine koy.
 		System.out.println(sayi);
 
 	}
