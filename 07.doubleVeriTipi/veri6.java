c2f9bbd3e328e3647c0ceef4806187e9060f139a
Merve Karagulle <mervekrgll44@icloud.com>
2025-07-06 09:06:26 +0300

    Add files via upload

---
+++ 07.doubleVeriTipi/veri6.java
@@ -0,0 +1,12 @@
+
+public class veri6 {
+
+	public static void main(String[] args) {
+		// double veri tipi daha hassasiyetli alanlarda kullanılan bir ondalıklı sayı tutan veri tipidir.
+		//virgülden sonra 17 basamağa kadar çıkabilmektedir.
+		double sayi=6.298969812582;
+		System.out.println(sayi);
+       
+	}
+
+}
